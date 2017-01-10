ae92215a1146ef035aad0e2ab1ec57a54bf13f17
Kyle <sully8437@gmail.com>
2017-01-10 10:24:01 -0600

    finished flux tempalte

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 var App = require('./components/App');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var AppAPI = require('./utils/appAPI.js');
+var appAPI = require('./utils/appAPI.js');
+
 
 ReactDOM.render(
     <App />,
